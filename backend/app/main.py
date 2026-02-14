5bbee314b3d8aa231601a49912925e2571feaee5
Siddharth Gautam <siddharthsinghsinde@gmail.com>
2026-02-15 03:08:36 +0530

    Update CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,16 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, Base
 import app.models
-from app.routers import employee,attendance
+from app.routers import employee, attendance
 
 
-app = FastAPI( title = "HMRS Lite API")
+app = FastAPI(title="HMRS Lite API")
 
 Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://h-m-r-s-project.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
